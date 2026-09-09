31d521fb4b43b5fddf0b2570eb1bf5b57159c658
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-09-09 17:38:07 +0200

    Implemented Circle Model

---
+++ AreaCalculatorApp/Models/CircleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculatorApp.Models
+{
+    public class CircleModel : IAreaCalculator
+    {
+        public double Radius { get; set; }
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+}
