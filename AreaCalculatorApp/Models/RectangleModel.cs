c4d430989c1a32f29930f380c0bbb559bfb660aa
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-09-09 17:34:18 +0200

    Implemented Rectangle Class

---
+++ AreaCalculatorApp/Models/RectangleModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculatorApp.Models
+{
+    public class RectangleModel : IAreaCalculator
+    {
+        public double Length { get; set; }
+
+        public double Height { get; set; }
+
+        public double CalculateArea()
+        {
+            return Length * Height;
+        }
+    }
+}
